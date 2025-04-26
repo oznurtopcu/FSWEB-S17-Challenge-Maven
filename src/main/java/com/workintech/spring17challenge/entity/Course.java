02633f1bdd8b9ca26fe9a40d37ec261238c87be3
oznurtopcu <topcuoznur121@gmail.com>
2025-04-26 23:39:06 +0300

    created course and grade classes

--- src/main/java/com/workintech/spring17challenge/entity/Course.java
+++ src/main/java/com/workintech/spring17challenge/entity/Course.java
@@ -1,7 +1,15 @@
 package com.workintech.spring17challenge.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Course {
+    private Integer id;
     private String name;
-    private int credit;
+    private Integer credit;
     private Grade grade;
 }
--- src/main/java/com/workintech/spring17challenge/entity/Grade.java
+++ src/main/java/com/workintech/spring17challenge/entity/Grade.java
@@ -1,5 +1,12 @@
 package com.workintech.spring17challenge.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Grade {
     private int coefficient;
     private String note;
